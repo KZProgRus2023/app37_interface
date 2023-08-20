3c88e498dd29749262e2489bc3f5fa1d8bfd3e19
Кирилл Желиховский <KirillZhelihovskij@rambler.ru>
2023-08-20 10:42:18 +0300

    Добавьте файлы проекта.

---
+++ создание и использование интерфейса/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace app37_interface
+{
+    interface ICar
+    {
+        int Speed { get; set; }
+        void GetInfo();
+    }
+    interface IPrice
+    {
+        double Price();
+    }
+    public class Ferrari : ICar, IPrice
+    {
+        private int spd; // скорость (поле)
+        public int Speed // скорость (свойство)
+        {
+            get { return spd; }
+            set { spd = value; }
+        }
+        // Конкретная реализация интерфейса в классе:
+        public void GetInfo()
+        {
+            Speed = 250;
+            Console.WriteLine("Это суперкар Ferrari @ +
+            "с макс. скоростью {0}", Speed);
+        }
+        // Конкретная реализация интерфейса в классе:
+        public double IPrice()
+        {
+            Console.WriteLine("\nЦена автомобиля 20000");
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Создание и применение " +
+            "интерфейса");
+            Ferrari fr = new Ferrari();
+            fr.GetInfo();
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
